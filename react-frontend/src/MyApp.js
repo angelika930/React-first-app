f43182592b6f19e4a318c099cab3382a6defa567
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-09-25 09:32:52 -0700

    added handle Change function

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,26 +1,9 @@
 import React, {useState} from "react";
 import Table from "./Table";
-
+import Form from './Form';
 
 function MyApp() {
-	const [characters, setCharacters] = useState([
-    	{
-      		name: "Charlie",
-      		job: "Janitor",
-    	},
-    	{
-      		name: "Mac",
-      		job: "Bouncer",
-    	},
-   	{
-      		name: "Dee",
-      		job: "Aspiring actress",
-    	},
-    	{
-      		name: "Dennis",
-      		job: "Bartender",
-    	},
-  ]);
+	const [characters, setCharacters] = useState([]);
 	function removeOneCharacter (index) {
 		const updated = characters.filter((character,i) => {
 			return i !== index
@@ -31,6 +14,7 @@ function MyApp() {
     <div className = "container">
         <Table characterData={characters}
 	  	removeCharacter={removeOneCharacter} />
+	<Form />
     </div>
   );
 }
