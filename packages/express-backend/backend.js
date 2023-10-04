887da63c3dc8e2993110b79b241272d42f9ff203
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-04 09:25:14 -0700

    added type in json and added state to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,43 @@
 import express from "express";
 
+const users = { 
+    users_list : [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
+ 
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.get('/users', (req, res) => {
+    res.send(users);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
